8f96c3a1cc926c9ffaa8e224153bf4f225d6cb6b
Paul M. Heider <paul.heider@gmail.com>
2017-07-19 12:21:21 -0400

    new travis test script based on AndreMiras/PyWallet

---
+++ travis/script_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Linux specific Travis script
+
+# exits on fail
+set -e
+
+# creates & uses the virtualenv
+virtualenv --python=python2.7 --no-site-packages venv
+source venv/bin/activate
+
+# installs requirements
+pip install --install-option="--no-cython-compile" $(grep Cython requirements.txt)
+pip install -r tests/requirements.txt
+pip install -r requirements.txt
+
+# runs tests
+python -m pytest tests
+#isort --check-only --recursive ./
+#flake8 ./
+#coverage run --source=./ -m unittest discover --start-directory=./
